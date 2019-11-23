86fa27c385afb6809e5137151a55082ade11cfad
Kendall Weihe <kendallweihe@gmail.com>
2019-11-23 14:52:41 -0500

    added basic projects, hello world and json

---
+++ json-reader-writer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace json_reader_writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = args[0];
+            var text = File.ReadAllText(filename);
+            var jobject = JObject.Parse(text);
+
+            Console.WriteLine(jobject["hello"]);
+        }
+    }
+}
